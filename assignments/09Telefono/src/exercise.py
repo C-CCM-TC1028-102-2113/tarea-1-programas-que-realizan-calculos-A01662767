7e4e3f2daa927b91698b85d0a9754d37291978c8
A01662767 <89958847+A01662767@users.noreply.github.com>
2021-09-01 23:51:27 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("TELEFONO")
+    men = input("DAME LOS MENSAJES: ")
+    meg = input("DAME LOS MEGAS: ")
+    min = input("DAME LOS MINUTOS: ")
+    total = .80 * (men + meg + min) 
     #Leer los datos
+    print("TU TOTAL ES DE:", total)
     pass
 
 
