2a5a30b190f5b29867e8cf09ca1893822bc84882
A01662767 <89958847+A01662767@users.noreply.github.com>
2021-09-01 23:39:41 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    print ("CARACOL")
+    minuto = input("INGRESA LOS MINUTOS: ")
+    d = minuto * 33
+    print("EL CARACOL RECORRE:", d)
 
 if __name__ == '__main__':
     main()
